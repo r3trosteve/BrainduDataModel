9f9192762032347b84f9651a9e1be9f6c0879070
Steve Schofield <stevebschofield@gmail.com>
2014-10-14 06:26:25 +0100

    Initial Commit

---
+++ BrainduDataModel/BrainduDataModel.h
@@ -0,0 +1,19 @@
+//
+//  BrainduDataModel.h
+//  BrainduDataModel
+//
+//  Created by Steven Schofield on 14/10/2014.
+//  Copyright (c) 2014 Double Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BrainduDataModel.
+FOUNDATION_EXPORT double BrainduDataModelVersionNumber;
+
+//! Project version string for BrainduDataModel.
+FOUNDATION_EXPORT const unsigned char BrainduDataModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrainduDataModel/PublicHeader.h>
+
+
